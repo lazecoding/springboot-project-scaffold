58ee6226f2f6fcddd4506a13d324cd7b98d36202
lazecoding <15062270668@163.com>
2022-07-06 20:52:35 +0800

    feat:ApplicationRunner 增加 try-catch

--- src/main/java/lazecoding/project/listener/ProjectStartupRunner.java
+++ src/main/java/lazecoding/project/listener/ProjectStartupRunner.java
@@ -1,5 +1,7 @@
 package lazecoding.project.listener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -12,8 +14,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProjectStartupRunner implements ApplicationRunner {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProjectStartupRunner.class);
+
     @Override
     public void run(ApplicationArguments args) {
-        System.out.println("初始化...");
+        try {
+            System.out.println("初始化...");
+        } catch (Exception e) {
+            logger.error("ApplicationRunner Exception", e);
+        }
     }
 }
