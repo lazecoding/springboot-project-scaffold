fe51e766d35f4fb8f50b852f1efee73ef4c3a3fa
lazecoding <15062270668@163.com>
2023-03-28 21:58:34 +0800

    feat: LocalFileUtil - delete、writeStringToFile

--- src/main/java/lazecoding/project/util/LocalFileUtil.java
+++ src/main/java/lazecoding/project/util/LocalFileUtil.java
@@ -7,8 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
 
-import java.io.File;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -79,5 +84,92 @@ public class LocalFileUtil {
      */
     public interface LineProcessor {
         void readLines(List<String> list);
+    }
+
+    /**
+     * 删除文件或目录
+     *
+     * @param path
+     */
+    public static boolean delete(String path) {
+        boolean isSuccess = false;
+        try {
+            Files.delete(Paths.get(path));
+            isSuccess = true;
+        } catch (NoSuchFileException x) {
+            isSuccess = true;
+            System.err.format("%s: no such" + " file or directory%n", path);
+        } catch (DirectoryNotEmptyException x) {
+            System.err.format("%s not empty%n", path);
+            isSuccess = false;
+        } catch (IOException x) {
+            // File permission problems are caught here.
+            System.err.println(x);
+            isSuccess = false;
+        }
+        return isSuccess;
+    }
+
+    /**
+     * 写内容到文件里
+     */
+    public static boolean writeStringToFile(String path, String content) {
+        return writeStringToFile(path, content, false);
+    }
+
+    /**
+     * 写内容到文件里
+     */
+    public static boolean writeStringToFile(String path, String content, boolean append) {
+        boolean isSuccess = false;
+        try {
+            OutputStream out = null;
+            try {
+                out = openOutputStream(new File(path), append);
+                write(content, out, StandardCharsets.UTF_8);
+                out.close();
+            } finally {
+                closeQuietly(out);
+            }
+            isSuccess = true;
+        } catch (Exception e) {
+            isSuccess = false;
+        }
+        return isSuccess;
+    }
+
+    private static FileOutputStream openOutputStream(File file, boolean append) throws IOException {
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                throw new IOException("File '" + file + "' exists but is a directory");
+            }
+
+            if (!file.canWrite()) {
+                throw new IOException("File '" + file + "' cannot be written to");
+            }
+        } else {
+            File parent = file.getParentFile();
+            if (parent != null && !parent.mkdirs() && !parent.isDirectory()) {
+                throw new IOException("Directory '" + parent + "' could not be created");
+            }
+        }
+
+        return new FileOutputStream(file, append);
+    }
+
+    private static void write(String data, OutputStream output, Charset encoding) throws IOException {
+        if (data != null) {
+            output.write(data.getBytes(encoding));
+        }
+
+    }
+
+    private static void closeQuietly(Closeable closeable) {
+        try {
+            if (closeable != null) {
+                closeable.close();
+            }
+        } catch (IOException var2) {
+        }
     }
 }
