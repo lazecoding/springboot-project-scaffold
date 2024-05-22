a9507fb5b1947847d71cbbbcfe94359017c46553
lazecoding <15062270668@163.com>
2024-05-22 22:44:05 +0800

    perf: try-catch-finally 写法

--- src/main/java/lazecoding/project/util/lucene/LuceneSimilaritySearch.java
+++ src/main/java/lazecoding/project/util/lucene/LuceneSimilaritySearch.java
@@ -17,6 +17,8 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import org.apache.lucene.store.Directory;
 
+import java.io.IOException;
+
 
 /**
  * 相似度检索
@@ -25,50 +27,53 @@ import org.apache.lucene.store.Directory;
  */
 public class LuceneSimilaritySearch {
 
-    public static void main(String[] args) throws Exception {
-        // 创建内存型索引
-        Directory index = new ByteBuffersDirectory();
+    public static void main(String[] args) {
+        IndexWriter writer = null;
+        try {
+            // 创建内存型索引
+            Directory index = new ByteBuffersDirectory();
 
-        // 创建IndexWriter
-        IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
-        IndexWriter writer = new IndexWriter(index, config);
+            // 创建IndexWriter
+            IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+            writer = new IndexWriter(index, config);
 
-        // 添加文档到索引
-        addDocument(writer, "1", "Lucene is a full-text search library.");
-        addDocument(writer, "2", "Lucene is written in Java.");
-        addDocument(writer, "3", "Java is written in Lucene.");
-        addDocument(writer, "4", "Lucene is written in Golang.");
-        addDocument(writer, "5", "Lucene is powerful.");
-        addDocument(writer, "6", "PHP is Good");
+            // 添加文档到索引
+            Document doc = new Document();
+            // 仅存储
+            doc.add(new StoredField("id", "id-1"));
+            // 检索 + 存储
+            doc.add(new TextField("content", "Lucene", Field.Store.YES));
+            writer.addDocument(doc);
 
-        // 提交写入
-        writer.close();
+            // 提交写入
+            writer.close();
 
-        // 构建查询
-        QueryParser parser = new QueryParser("content", new StandardAnalyzer());
-        Query query = parser.parse("Lucene Java.");
+            // 构建查询
+            QueryParser parser = new QueryParser("content", new StandardAnalyzer());
+            Query query = parser.parse("Lucene Java.");
 
-        // 执行查询
-        IndexReader indexReader = DirectoryReader.open(index);
-        IndexSearcher searcher = new IndexSearcher(indexReader);
-        TopDocs results = searcher.search(query, 10);
+            // 执行查询
+            IndexReader indexReader = DirectoryReader.open(index);
+            IndexSearcher searcher = new IndexSearcher(indexReader);
+            TopDocs results = searcher.search(query, 10);
 
-        // 输出查询结果
-        for (ScoreDoc scoreDoc : results.scoreDocs) {
-            Document doc = searcher.doc(scoreDoc.doc);
-            System.out.println("Score: " + scoreDoc.score + ", Document: " + doc);
+            // 输出查询结果
+            for (ScoreDoc scoreDoc : results.scoreDocs) {
+                System.out.println("Score: " + scoreDoc.score + ", Document: " + searcher.doc(scoreDoc.doc));
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
         }
+
+
     }
 
-    /**
-     * 添加文档
-     */
-    private static void addDocument(IndexWriter writer, String id, String content) throws Exception {
-        Document doc = new Document();
-        // 仅存储
-        doc.add(new StoredField("id", id));
-        // 检索 + 存储
-        doc.add(new TextField("content", content, Field.Store.YES));
-        writer.addDocument(doc);
-    }
 }
