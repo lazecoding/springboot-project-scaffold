2ae7c09090647f3eca5184659c6b82cf9a8a1cb6
lazecoding <15062270668@163.com>
2025-01-19 17:33:04 +0800

    feat：RQueue

--- src/main/java/lazecoding/project/mock/ConcurrentMockController.java
+++ src/main/java/lazecoding/project/mock/ConcurrentMockController.java
@@ -1,12 +1,18 @@
 package lazecoding.project.mock;
 
 import lazecoding.project.common.mvc.ResultBean;
+import lazecoding.project.common.util.JsonUtil;
+import lazecoding.project.common.util.cache.RedissonClientUtil;
 import lazecoding.project.common.util.concurrent.AtomicNumber;
 import lazecoding.project.common.util.concurrent.Locker;
+import org.redisson.api.RQueue;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("mock/concurrent")
@@ -66,5 +72,31 @@ public class ConcurrentMockController {
         resultBean.setMessage("pong");
         return resultBean;
     }
+
+    @GetMapping(value = "queue")
+    @ResponseBody
+    public ResultBean queue() {
+        ResultBean resultBean = ResultBean.getInstance();
+        RedissonClient redisson = RedissonClientUtil.getRedissonClient();
+        // 获取队列对象
+        RQueue<String> queue = redisson.getQueue("myQueue");
+        // 入队操作
+        queue.offer("element1");
+        queue.offer("element2");
+        queue.offer("element3");
+        // 获取队列大小
+        long size = queue.size();
+        System.out.println("队列大小: " + size);
+        // 出队操作
+        String firstElement = queue.poll();
+        System.out.println("出队元素: " + firstElement);
+        List<String> all = queue.readAll();
+        System.out.println("全部元素: " + JsonUtil.GSON.toJson(all));
+        // 移除 mock 数据
+        queue.delete();
+        resultBean.setMessage("queue");
+        return resultBean;
+    }
+
 
 }
